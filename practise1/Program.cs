96111b1114f504d65c401c0454dc62bd64e4ad42
Mattias Öigaard <mattias.oigaard@gmail.com>
2019-10-25 08:51:17 +0200

    Add project files.

---
+++ practise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace practise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Write your name: ");
+                string input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@echo: " + input);
+                    continue;
+                }
+
+                    break;
+
+                                                                                        
+            }
+        }
+    }
+}
